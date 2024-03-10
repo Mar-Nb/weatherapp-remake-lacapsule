ce65af1b491cd28b7569f7ae53d667b8ab527d50
Mar-Nb <niombela.martin@gmail.com>
2024-03-10 19:14:25 +0100

    chore: Suppression des fichiers non utilisés

--- apps/server/src/cities/cities.module.ts
+++ apps/server/src/cities/cities.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { CitiesService } from './cities.service';
-import { CitiesController } from './cities.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { City, CitySchema } from './city.schema';
 
@@ -8,7 +7,6 @@ import { City, CitySchema } from './city.schema';
   imports: [
     MongooseModule.forFeature([{ name: City.name, schema: CitySchema }]),
   ],
-  controllers: [CitiesController],
   providers: [CitiesService],
   exports: [CitiesService],
 })
